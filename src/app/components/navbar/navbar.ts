b13fc83724431111727b129d754bf23ccf380ff2
Jeffry Gonzalez <jeff@hypertheory.com>
2025-10-06 15:43:05 -0400

    navbar done for now

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,44 +1,27 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NavbarList } from './navbar-list';
+import { NavbarCenter } from './components/navbar-center';
+import { NavbarSide } from './components/navbar-side';
+import { NavbarLink } from './types';
+import { NavbarHamburger } from './components/navbar-hamburger';
 
 @Component({
   selector: 'app-nav-bar',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterLink, NavbarList],
+  imports: [RouterLink, NavbarCenter, NavbarSide, NavbarHamburger],
   template: `
     <div class="navbar bg-base-100 shadow-sm">
       <div class="navbar-start">
         <div class="dropdown">
           <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+            <app-navbar-hamburger-menu />
           </div>
-          <ul
-            tabindex="0"
-            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <app-navbar-list [links]="links()" />
-          </ul>
+          <app-navbar-side [links]="links()" />
         </div>
         <a routerLink="/" class="btn btn-ghost text-xl">Applied Angular</a>
       </div>
       <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal px-1">
-          <app-navbar-list [links]="links()" />
-        </ul>
+        <app-navbar-center [links]="links()" />
       </div>
       <div class="navbar-end">
         <a class="btn">Button</a>
@@ -48,7 +31,7 @@ import { NavbarList } from './navbar-list';
   styles: ``,
 })
 export class Navbar {
-  links = signal([
+  links = signal<NavbarLink[]>([
     {
       label: 'Dashboard',
       path: '/dashboard',

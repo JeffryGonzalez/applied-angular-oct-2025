089b22259791f81532ad7e8af679a47397b8c325
Jeffry Gonzalez <jeff@hypertheory.com>
2025-10-06 13:51:11 -0400

    for eddie

--- src/app/components/navbar.ts
+++ src/app/components/navbar.ts
@@ -39,7 +39,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
             <li><a>Item 3</a></li>
           </ul>
         </div>
-        <a class="btn btn-ghost text-xl">daisyUI</a>
+        <a class="btn btn-ghost text-xl">Applied Angular</a>
       </div>
       <div class="navbar-center hidden lg:flex">
         <ul class="menu menu-horizontal px-1">
